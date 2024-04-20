e147d03ae50c0e23238a3d4b65b6f2d69063f239
Oleksandr <lysakova@yahoo.com>
2024-04-20 07:24:44 +0200

    fix: repeate function

--- functions/task3-closure/repeatFunction.js
+++ functions/task3-closure/repeatFunction.js
@@ -1,10 +1,23 @@
-const repeatFunction = (fn, num) => () => {
+const repeatFunction = (fn, num) => {
   let quantity = num >= 0 ? num : Infinity;
+  let stopped = false;
 
-  while (quantity > 0) {
-    fn();
-    quantity--;
-  }
+  const stop = () => {
+    stopped = true;
+  };
+
+  const repeat = () => {
+    const intervalId = setInterval(() => {
+      if (quantity > 0 && !stopped) {
+        fn();
+        quantity--;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 0);
+  };
+
+  return { repeat, stop };
 };
 
 module.exports = repeatFunction;
